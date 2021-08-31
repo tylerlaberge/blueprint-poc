ac0c2af0f2e775bf18143287fe03351ab9cacd87
tylerlaberge <tylerlaberge@protonmail.com>
2021-08-30 20:02:43 -0400

    Added draggable anchor elements to blueprint-wire

--- src-angular/src/app/blueprint/blueprint-wire/blueprint-wire.component.ts
+++ src-angular/src/app/blueprint/blueprint-wire/blueprint-wire.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import * as LeaderLine from 'leader-line-new';
 import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
@@ -9,7 +9,7 @@ import { PortControlComponent } from '../port/port-control/port-control.componen
   templateUrl: './blueprint-wire.component.html',
   styleUrls: ['./blueprint-wire.component.scss']
 })
-export class BlueprintWireComponent implements OnInit, OnDestroy {
+export class BlueprintWireComponent implements OnInit, AfterViewInit, OnDestroy {
 
     private _subscriptions: Subscription[] = [];
     private _leaderLine$: BehaviorSubject<LeaderLine | null> = new BehaviorSubject<LeaderLine | null>(null);
@@ -22,21 +22,32 @@ export class BlueprintWireComponent implements OnInit, OnDestroy {
     @Input() set output(value: PortControlComponent) { this._outputPort$.next(value); }
     @Input() set refresh(value: Observable<void>) { this._refresh$ = value; }
 
+    @ViewChild("start") startAnchor!: ElementRef;
+    @ViewChild("end") endAnchor!: ElementRef;
+
     ngOnInit() {
       this._subscriptions.push(this._leaderLine$.subscribe(leaderLine => leaderLine?.position()));
       this._subscriptions.push(this._refresh$.subscribe(() => this._leaderLine$.getValue()?.position()));
+    }
+
+    ngAfterViewInit() {
       this._subscriptions.push(combineLatest([this._inputPort$, this._outputPort$])
           .pipe(
               filter(([input, output]) => !!input && !!output),
           ).subscribe(([input, output]) => {
-              this._leaderLine$.getValue()?.remove();
-              this._leaderLine$.next(this.drawLeaderLine(output!.getPortElement(), input!.getPortElement()))
+            input!.getPortElement().appendChild(this.endAnchor.nativeElement);
+            output!.getPortElement().appendChild(this.startAnchor.nativeElement);
           }));
+      this._leaderLine$.next(this.drawLeaderLine(this.startAnchor.nativeElement, this.endAnchor.nativeElement));
     }
 
     ngOnDestroy() {
       this._subscriptions.forEach(sub => sub.unsubscribe());
       this._leaderLine$.getValue()?.remove();
+    }
+
+    handleDrag() {
+      this._leaderLine$.getValue()?.position();
     }
 
     private drawLeaderLine(start: HTMLElement, end: HTMLElement): LeaderLine {
